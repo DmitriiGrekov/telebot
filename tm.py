577d7047e8d5898e43d74471b1aac34c6dc2cf51
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 22:27:49 +0900

    ‘new2’

--- tm.py
+++ tm.py
@@ -5,20 +5,19 @@ bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	user_markup=telebot.types.ReplyKeyboardMarkup()
-	user_markup.row('/start','/stop')
+        bot.send_message(message.chat.id,'Ринат вор? --> Да/Нет')
+	
 @bot.message_handler(content_types=['text'])
 def mes_handler(message):
         if message.text=="Да":
                 bot.send_message(message.chat.id,'Красава,уважаю')
         elif message.text=='Нет':
-                bot.send_message(message.chat.id,'Рустик,ты что-ли')
+                bot.send_message(message.chat.id,'Ты случайно не ринат')
+                photo = open('/tmp/photo.png', 'rb')
+                bot.send_photo(message.chat.id, photo)
                 
         else:
-                bot.send_message(message.chat.id,'Ты настолько тупой что даже не можешь правильно написать.Это точно Руслан' )
-        
-
-@
+                bot.send_message(message.chat.id,"Попробуй ответить снова.")
 
 bot.polling()
 
