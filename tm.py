e3c7cb11014892e902592c7b162df04ca6c5be36
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 13:06:26 +0900

    ‘new’

--- tm.py
+++ tm.py
@@ -1,4 +1,8 @@
 import telebot
 bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id,"Test")
+
 bot.send_message(264212583,"test")
