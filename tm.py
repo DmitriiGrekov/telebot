5b298bb45ff00478d1540251fb03f10a3e5e82b4
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 13:45:44 +0900

    'news2'

--- tm.py
+++ tm.py
@@ -1,5 +1,8 @@
 import telebot
+from flask import Flask,request
+import os
 bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
+server = Flask(__name__)
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -7,4 +10,8 @@ def handle_text(message):
 
 bot.send_message(264212583,"test")
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True)
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
