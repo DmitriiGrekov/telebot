8222dc1306a522da81e7b00d7d724efc8d59e240
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 20:52:27 +0900

    'new3'

--- tm.py
+++ tm.py
@@ -3,21 +3,19 @@ import telebot
 bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 
 
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    if meesage.text == "a":
-        bot.send_message(message.chat.id,"Б")
-    elif message.text == 'б':
-        bot.send_message(message.chat.id,'А')
-    else:
-        bot.send_message(message.chat.id,'Ты не умеешь играть в эту игру')
-@bot.message_handler(commands=["start"])
-def handler_command(message):
-    bot.send_message(message.chat.id,"Welcome to my chat")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
 
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-bot.polling(none_stop=True)
+bot.polling()
+
+
 
 
 
