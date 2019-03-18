42cbef33a77d578a7d5cc49a5706f6bacebc88ca
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 13:59:06 +0900

    'new3'

--- tm.py
+++ tm.py
@@ -6,7 +6,12 @@ server = Flask(__name__)
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id,"Test")
+    if meesage.text == "a":
+        bot.send_message(message.chat.id,"Б")
+    elif message.text == 'б':
+        bot.send_message(message.chat.id,'А')
+    else:
+        bot.send_message(message.chat.id,'Ты не умеешь играть в эту игру')
 @bot.message_handler(commands=["start"])
 def handler_command(message):
     bot.send_message(message.chat.id,"Welcome to my chat")
