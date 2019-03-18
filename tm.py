67e8af89da7b095c766f1ba79c70e3e1029d1e96
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 21:51:01 +0900

    'new3'

--- tm.py
+++ tm.py
@@ -17,7 +17,14 @@ def mes_handler(message):
                 bot.send_message(message.chat.id,'Ты настолько тупой что даже не можешь правильно написать.Это точно Руслан' )
         
 
-
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Текущая дата
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
+    markup = create_calendar(now.year,now.month)
+    bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
 
 bot.polling()
 
