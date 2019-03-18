0b5e111859474cbca1b883da709819f138d337cc
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-18 21:55:02 +0900

    'new3'

--- tm.py
+++ tm.py
@@ -2,7 +2,14 @@ import telebot
 
 bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 
-
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Текущая дата
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
+    markup = create_calendar(now.year,now.month)
+    bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -17,14 +24,7 @@ def mes_handler(message):
                 bot.send_message(message.chat.id,'Ты настолько тупой что даже не можешь правильно написать.Это точно Руслан' )
         
 
-@bot.message_handler(commands=['calendar'])
-def get_calendar(message):
-    now = datetime.datetime.now() #Текущая дата
-    chat_id = message.chat.id
-    date = (now.year,now.month)
-    current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
-    markup = create_calendar(now.year,now.month)
-    bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
+@
 
 bot.polling()
 
