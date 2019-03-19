42a8dde5890659e93fbd099993e7633640d1cb66
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-19 20:25:15 +0900

    ‘newse’

--- tm.py
+++ tm.py
@@ -5,6 +5,20 @@ def firest(message):
     key=telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True)
     key.row("Да","Нет")
     bot.send_message(message.chat.id,"Руслан гей",reply_markup=key)
+@bot.message_handler(content_types=["text"])
+def send_message(message):
+    if message.text == "Да":
+        bot.send_message(message.chat.id,'Красава,Уважаю!')
+        photo = open('yes.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+        bot.send_message(message.chat.id,"Руслан гей")
+    elif message.text == 'Нет':
+        bot.send_message(message.chat.id,'Рустик,ты что-ли!')
+        photo = open('no.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+        bot.send_message(message.chat.id,"Руслан гей")
+    
+        
 
 
 if __name__=="__main__":
