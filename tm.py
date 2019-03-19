f8cf40d06a1f90b0b700bc51f73297dfead75d99
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-19 20:11:58 +0900

    ‘newse’

--- tm.py
+++ tm.py
@@ -2,7 +2,7 @@ import telebot
 bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 @bot.message_handler(commands=["start"])
 def firest(message):
-    key=telebot.types.ReplyKeyboardMarkup(True,False)
+    key=telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True)
     key.row("Да","Нет")
     bot.send_message(message.chat.id,"Руслан гей",reply_markup=key)
 
