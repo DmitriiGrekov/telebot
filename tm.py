999396d388e9c90ce288e1b706ddeb5ac59ec67e
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-19 20:02:14 +0900

    ‘newse’

--- tm.py
+++ tm.py
@@ -1,5 +1,5 @@
 import telebot
-bot=telebot.Telebot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
+bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
 @bot.message_handler(commands=["start"])
 def firest(message):
     key=telebot.types.ReplyKeyboardMarkup(True,False)
@@ -8,4 +8,4 @@ def firest(message):
 
 
 if __name__=="__main__":
-    bot.polling(none_stop=True,interval=0,timeout=20)
+    bot.polling()
