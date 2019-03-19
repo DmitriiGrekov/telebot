10240c9a880738bc2e9932444c7f326abfa917ad
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-19 19:49:31 +0900

    ‘newse’

--- tm.py
+++ tm.py
@@ -1,27 +1,11 @@
 import telebot
-
-bot = telebot.TeleBot("797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-        bot.send_message(message.chat.id,'Ринат вор? --> Да/Нет')
-	
-@bot.message_handler(content_types=['text'])
-def mes_handler(message):
-        if message.text=="Да":
-                bot.send_message(message.chat.id,'Красава,уважаю')
-        elif message.text=='Нет':
-                bot.send_message(message.chat.id,'Ты случайно не ринат')
-                photo = open('rinat.jpg', 'rb')
-                bot.send_photo(message.chat.id, photo)
-                
-        else:
-                bot.send_message(message.chat.id,"Попробуй ответить снова.")
-
-bot.polling()
+bot=telebot.Telebot(token="797549243:AAE-nR3eITIaO-OberdmXRi2Poywlpj3_0w")
+@bot.message_handler(commands=["start"])
+def firest(message):
+    key=telebot.types.ReplyKeyboardMarkup(True,False)
+    key.row("Да","Нет")
+    bot.send_message(message.chat.id,"Руслан гей",reply_markup=key)
 
 
-
-
-
+if __name__=="__main__":
+    bot.polling(none_stop=True,interval=0,timeout=20)
