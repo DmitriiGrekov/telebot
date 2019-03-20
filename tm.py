23f364bce670aaf12141b60bc6f004b5639280af
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-20 20:27:40 +0900

    ‘new’

--- tm.py
+++ tm.py
@@ -41,7 +41,7 @@ def translated(message):
     TEXT=message.text
     LANg=lang1
     r=requests.post(url,data={'key':key,'text':TEXT,'lang':LANg})
-    bot.send_message(message.chat,id,eval(r.text)['text'])
+    bot.send_message(message.chat,id,*(eval(r.text))['text'])
     
     
     
