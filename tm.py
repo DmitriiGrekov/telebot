390e7b0fa2b4d0e2bc5fb0cff0e7207e4d521d06
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-20 22:10:32 +0900

    ‘new’

--- tm.py
+++ tm.py
@@ -68,23 +68,19 @@ def translated_menu_3(message):
         bot.register_next_step_handler(send,translated)
 
 def translated(message):
-    key=telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True)
-    key.row("/start",)
-    send=bot.send_message(message.chat.id,"",reply_markup=key)
-    if message.text=="/start":
-        bot.register_next_step_handler(send,firest)
-    else:
+    global lang1
+    global lang2
+
         
         
-        url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
-        key='trnsl.1.1.20190201T172728Z.34034e93ef318814.4cd85f71122011aa48770690493d232d5ff78c60'
-    
-        TEXT=message.text
-        LANg=lang1+"-"+lang2
-        r=requests.post(url,data={'key':key,'text':TEXT,'lang':LANg})
-        bot.send_message(message.chat.id,*eval(r.text)['text'])
-        
-        bot.register_next_step_handler(send,translated)
+    url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
+    key='trnsl.1.1.20190201T172728Z.34034e93ef318814.4cd85f71122011aa48770690493d232d5ff78c60'    
+    TEXT=message.text
+    LANg=lang1+"-"+lang2
+    r=requests.post(url,data={'key':key,'text':TEXT,'lang':LANg})
+    bot.send_message(message.chat.id,*eval(r.text)['text'])
+    send=bot.send_message(message.chat.id,"Введите фразу")
+    bot.register_next_step_handler(send,translated)
     
     
     
