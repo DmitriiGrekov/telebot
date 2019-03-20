21fa2edaa96061e7c4afaaf99095a698c9ded56a
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-20 21:05:42 +0900

    ‘new’

--- tm.py
+++ tm.py
@@ -18,6 +18,7 @@ def translated_menu_1(message):
         send=bot.send_message(message.chat.id,"Выберите с какой язык переводить",reply_markup=key)
         bot.register_next_step_handler(send,translated_menu_2)
 def translated_menu_2(message):
+    global lang1
     if message.text == 'Рус':
         lang1='ru'
         key=telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True,resize_keyboard=True)
@@ -44,6 +45,7 @@ def translated_menu_2(message):
         bot.register_next_step_handler(send,translated_menu_3)
     
 def translated_menu_3(message):
+    global lang2
     bot.send_message(message.chat.id,lang1)
     if message.text == 'Рус':
         lang2='ru'
@@ -64,14 +66,9 @@ def translated_menu_3(message):
         bot.register_next_step_handler(send,translated)
 
 def translated(message):
-    url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
-    key='trnsl.1.1.20190201T172728Z.34034e93ef318814.4cd85f71122011aa48770690493d232d5ff78c60'
-    TEXT=message.text
-    Lang=lang1 + "-"+lang2
-    parametrs={'key':key,'lang':'','text':TEXT}
-    
-    r=requests.post(url,)
-    bot.send_message(message.chat.id,lang2)
+    global lang1
+    global lang2
+    bot.send_message(message.chat.id,lang1)
     
     
     
